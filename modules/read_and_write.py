8a575f617c29838a7e0cbe11efca65e00a617028
Haaaaron <aaron.haarti@protonmail.com>
2023-10-05 14:15:25 +0300

    wilson action codes work

--- modules/read_and_write.py
+++ modules/read_and_write.py
@@ -1,8 +1,42 @@
 import sys
 import os
 import pandas as pd
+import numpy as np
 
-def load_data_file(file, data_line):
+def load_data_file_complex(file, data_line):
+    f = open(file,'r')
+    data = []
+    beginRead = False
+    beta = None
+    for line in f:
+        if not beginRead:
+            if line.startswith('beta'):
+                beta = str(line.split()[1])
+            if line.startswith('twist_coeff'):
+                twist_c = str(line.split()[1])
+            if line.startswith('MEASURE start'):
+                beginRead = True
+        elif line.startswith('MEASURE end'):
+            beginRead = False
+        else:
+            if line.startswith(data_line): 
+                line = line.split(':')[1]
+                numbers = [x for x in line.split(",")[:-1]]
+                #print(numbers)
+                complex_converted = []
+                for x in numbers:
+                    num = x.split()
+                    complex_converted.append(complex(float(num[0]),float(num[1])))
+                    #complex(num[0])
+                #print(complex_converted)
+                data.append(complex_converted)
+
+
+    data = pd.DataFrame(data[1:],columns=data[0])
+    name = beta + " " + twist_c
+    return name,data
+
+def load_data_file_real(file, data_line):
     f = open(file,'r')
     data = []
     beginRead = False
@@ -21,31 +55,24 @@ def load_data_file(file, data_line):
             if line.startswith(data_line):
                 
                 line = line.split(':')[1]
-                #If data is complex then do a little error handling
-                try:
-                    numbers = [x for x in line.split(",")[:-1]]
-                    #print(numbers)
-                    complex_converted = []
-                    for x in numbers:
-                        num = x.split()
-                        complex_converted.append(complex(float(num[0]),float(num[1])))
-                        #complex(num[0])
-                    #print(complex_converted)
-                    data.append(complex_converted)
-                except:
-                    data.append(line.split())
+                #print(line.split())
+                data.append(line.split())
 
-    data = pd.DataFrame(data[1:],columns=data[0])
+    data = pd.DataFrame(data[1:],columns=data[0],dtype=np.float32)
     name = beta + " " + twist_c
     return name,data
 
-def load_from_folder(folder,data_line):
+def load_from_folder(folder,data_line,real_or_complex):
     data_per_file = {}
-    
+    if real_or_complex == "real":
+        load_func = load_data_file_real
+    else:
+        load_func = load_data_file_complex
+        
     for root,dirs,files in os.walk(folder):
         for name in files:
             #print(os.path.join(root,name))
-            name,data = load_data_file(os.path.join(root,name), data_line)
+            name,data = load_func(os.path.join(root,name), data_line)
             #data = data['sum'].to_numpy()
             #columns = list(data.columns.values)
             #columns = columns[-len(columns)//2:] + columns[:-len(columns)//2]
@@ -55,10 +82,12 @@ def load_from_folder(folder,data_line):
             #print(data)
             data_per_file[name] = data
     #print(data_per_file['5.081616161616162 0'])
-    
+    myKeys = list(data_per_file.keys())
+    myKeys.sort()
+    data_per_file = {i: data_per_file[i] for i in myKeys}
     return data_per_file
 
 if __name__ == "__main__":
-    print(load_from_folder("./current_output/notwist","polyakov:"))
+    print(load_from_folder("./current_output/notwist","plaquette:","real"))
     #load_data_file("../current_output/")
     pass
---
+++ modules/wilson_action.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def create_average_action_figure(notwist,name_1,twist,name_2):
+    data = [[notwist,name_1],[twist,name_2]]
+    for i,(datas,name) in enumerate(data):
+        average_datas = {}
+        for simulation in datas:
+            average_sum = datas[simulation]['sum'].to_numpy().astype(float)
+            average_datas[simulation] = average_sum[50:].mean()
+        data[i][0] = average_datas
+    print(data)
+    for (datas,name) in data:
+        x = [float(x.split()[0]) for x in list(datas.keys())]
+        print(x)
+        y = list(datas.values())
+        plt.plot(x,y,'o--',label=name)
+        #plt.xticks([float(x) for x in datas[0].head()[1:-1]])
+    plt.title(r'Wilson action total average with respect to $\beta$')
+    plt.xlabel(r'$\beta$')
+    plt.ylabel(r'$\langle$ S $\rangle$')
+    plt.legend()
+    plt.show()
+    
+def create_z_index_heat_map(datas,cols=4,mean=False):
+    total = len(datas)
+    rows = total // cols
+    if total % cols != 0:
+        rows += 1
+        
+    position = range(1,total+1)
+    fig = plt.figure(1,figsize=(15,7))
+    for k,(name,data) in enumerate(datas.items()):
+        no_sum = data.drop(["sum"],axis=1)
+        ax = fig.add_subplot(rows,cols,position[k])
+        #ax.imshow(datas[name], cmap ="RdYlBu",aspect='auto')
+        delta = no_sum.to_numpy().max()-no_sum.to_numpy().min()
+        #print(data.mean().to_numpy())
+        #print(list(data.columns.values))
+        if mean:
+            ax.plot(no_sum[50:].columns.values,no_sum[50:].mean())
+        else:
+            sns.heatmap(no_sum,ax=ax)
+        ax.set_title(f"{name}")
+    plt.tight_layout(pad=0.4, w_pad=3, h_pad=1.0)
+    #plt.xticks([float(x) for x in datas[0].head()[1:-1]])
+    plt.show()
